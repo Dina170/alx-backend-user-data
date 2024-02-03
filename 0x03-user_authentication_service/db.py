ae128277715922cb17e8fa596924cc19cd31a67b
dina <dinaelhadidy966@gmail.com>
2024-02-03 16:38:02 -0500

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,14 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update the user’s attributes as passed
+           in the method’s arguments
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
